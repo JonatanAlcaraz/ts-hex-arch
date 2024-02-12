ccb50b2e1423783943a532d3197dacda3c8b9c6e
JonatanAlcaraz <jonatan.alcaraz9@gmail.com>
2024-02-12 04:24:50 -0300

    update product domain

--- src/product-management/domain/product.ts
+++ src/product-management/domain/product.ts
@@ -1,4 +1,5 @@
 export class Product {
+  productId: number;
   name: string;
   price: number;
 
@@ -6,7 +7,7 @@ export class Product {
     if (price < 0) {
       throw new Error("the price cannot be less than 0");
     }
-
+    this.productId = -1;
     this.name = name;
     this.price = price;
   }
